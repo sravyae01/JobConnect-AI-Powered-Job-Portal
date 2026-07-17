8d9e58210ef130229b48f28858a7459ec0841e0a
Sravya <sravyae01@gmail.com>
2026-07-17 17:12:52 +0530

    Fix CORS for Vercel

--- backend/jobportal/settings.py
+++ backend/jobportal/settings.py
@@ -174,12 +174,13 @@ CHANNEL_LAYERS = {
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "sravyapandu2004@gmail.com"
-EMAIL_HOST_PASSWORD = "suzzjgrmdvxitcvr"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
